b6dfea1bc81d067196a9560c4d2197aa6ec35f8d
Elierb <elierenebazin@gmail.com>
2024-10-01 14:22:28 +0100

    Article version of InversionBinnedParallel.py

--- snow_ice/CS_AK_2p_LARM/InversionBinnedParallel.py
+++ snow_ice/CS_AK_2p_LARM/InversionBinnedParallel.py
@@ -8,9 +8,12 @@ import pandas as pd
 import subprocess
 import matplotlib.pyplot as plt
 from itertools import chain
+from mpl_toolkits.basemap import Basemap
+from datetime import datetime
+import pickle
 import os
 import warnings
-import pickle
+import netCDF4 as nc
 
 
 def draw_map(m, scale=0.2):
@@ -39,6 +42,29 @@ def mask_observations(observations, inversion):
                 new_inversion[i][j] = np.nan
     return new_inversion
 
+def compute_rho_i(date):
+    reference_date = datetime(2010, 1, 1)
+    date_format = "%Y%m%d"
+    date_obj = datetime.strptime(date, date_format)
+    difference = date_obj - reference_date
+    nombre_de_jours = difference.days
+    
+    ice_type = nc.Dataset('/home/erb/masterproject/MSCI_project/snow_ice/carmen/icetype/icetype.nc').variables['Ice Type'][nombre_de_jours]
+
+    rho_MYI = 916.0
+    rho_FYI = 882.0
+
+    density_map = np.copy(ice_type)
+
+    for i in range(len(ice_type)):
+        for j in range(len(ice_type[0])):
+            if ice_type[i][j] == 3.0:
+                density_map[i][j] = rho_MYI
+            elif ice_type[i][j] == 2.0:
+                density_map[i][j] = rho_FYI
+    return density_map
+
+
 '''
 FROM DAILY DATA: 
 fb_path1 = "../carmen/daily_numpys/AK_CPOM/FB_interp_2016-2017_25km_20170309.npy"
@@ -46,30 +72,27 @@ fb_path2 = "../carmen/daily_numpys/CS2_LARM/FB_interp_2016-2017_25km_20170309.np
 
 '''
 def main(
-    fb_path1 = "/home/erb/masterproject/MSCI_project/snow_ice/carmen/non_interpolated_data/25km_ease_binned/AK_LARM/dailyFB_25km_2020-2021_season.pkl",
-    fb_path2 = "/home/erb/masterproject/MSCI_project/snow_ice/carmen/non_interpolated_data/25km_ease_binned/CS2_LARM/dailyFB_25km_2020-2021_season.pkl",
-    verbose=False,
-    date = ['20210414', '20210415', '20210416'],
-    month = 6,
-    minlat = -5000000.0/1000000, maxlat = 5000000.0/1000000,
-    minlon = -5000000.0/1000000, maxlon = 5000000.0/1000000,
-    parametrization = 1,
-    initial_cells = 6,
-    iterations_number = 10000,
-    verbosity = 5000,
-    independent_chains = 4,
-    temperature_levels = 1,
-    maximum_temperature = 2.0,
-    iterations_between_tempering_attempts = 10,
-    skipping = 1000,
-    thinning = 5,
-    render_map=False,
-    render_matrix=False,
-    render_observations=False,
-    render_median = False,
-    render_stddev = False,
-    render_histogram = False
+    month,
+    fb_path1,
+    fb_path2,
+    verbose,
+    date,
+    window,
+    minlat, maxlat,
+    minlon, maxlon,
+    parametrization,
+    initial_cells,
+    iterations_number,
+    verbosity,
+    independent_chains,
+    temperature_levels,
+    maximum_temperature,
+    iterations_between_tempering_attempts,
+    skipping,
+    thinning,
+    render_observations,
          ):
+
 
     if verbose:
         print("Starting inversion")
@@ -94,6 +117,10 @@ def main(
         ak.append(AK[date[k]])
         cs2.append(CS2[date[k]])
 
+    ak_std = np.load('/home/erb/masterproject/MSCI_project/snow_ice/CS_AK_2p_LARM/error_map/2020-2021/AK_Period_15j.npy')
+    cs2_std = np.load('/home/erb/masterproject/MSCI_project/snow_ice/CS_AK_2p_LARM/error_map/2020-2021/CS2_Period_15j.npy')
+
+
     fb1_filename = os.path.split(fb_path1)[1]
     fb2_filename = os.path.split(fb_path2)[1]
 
@@ -101,6 +128,34 @@ def main(
     grid_x = np.load("/home/erb/masterproject/MSCI_project/new_x_25km.npy")
     grid_y = np.load("/home/erb/masterproject/MSCI_project/new_y_25km.npy")
 
+    if date[0] == "20210408" :
+        ice_type = nc.Dataset('/home/erb/masterproject/MSCI_project/snow_ice/carmen/icetype/CDS_icetype/ice_type_nh_ease2-250_icdr-v3p0_202104151200.nc').variables['ice_type'][0][:,:]
+
+        ice_type = ice_type[::-1,:]
+        ice_type = ice_type[36:396, 36:396]
+
+        rho_MYI = 916.0
+        rho_FYI = 882.0
+
+        density_map = np.empty((360, 360))
+
+        for i in range(len(ice_type)):
+            for j in range(len(ice_type[0])):
+                if ice_type[i][j] == 3.0 :
+                    density_map[i][j] = rho_MYI
+                elif ice_type[i][j] == 2.0 or ice_type[i][j] == 4.0:
+                    density_map[i][j] = rho_FYI
+                else :
+                    density_map[i][j] = np.nan
+
+    else : 
+        density_map = compute_rho_i(date[7])
+
+
+    epsilon = 1e-4
+
+    ak_std[ak_std < epsilon] = 0.01
+    cs2_std[cs2_std < epsilon] = 0.01
 
     ak_observations = []
     cs2_observations = []
@@ -108,16 +163,21 @@ def main(
     for k in range(len(date)):
         for i in range (360):
             for j in range (360):
-                if not np.isnan(ak[k][i][j]):
-                    if i%2 == 0 and j%2 == 0:
-                        ak_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 1, ak[k][i][j], 0.01, month])
-                if not np.isnan(cs2[k][i][j]):
-                    if i%2 == 0 and j%2 == 0:
-                        cs2_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 0, cs2[k][i][j], 0.01, month])
+                if not np.isnan(ak[k][i][j]) and not np.isnan(density_map[i][j]):
+                    if ak_std[i][j] > 0:
+                        ak_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 1, ak[k][i][j], ak_std[i][j], month, density_map[i][j]])
+                    else : 
+                        ak_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 1, ak[k][i][j], 0.01, month, density_map[i][j]])
+
+                if not np.isnan(cs2[k][i][j]) and not np.isnan(density_map[i][j]):
+                    if cs2_std[i][j] > 0:
+                        cs2_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 0, cs2[k][i][j], cs2_std[i][j], month, density_map[i][j]])
+                    else :                         
+                        cs2_observations.append([grid_x[i][j]/1000000, grid_y[i][j]/1000000, 0, cs2[k][i][j], 0.01, month, density_map[i][j]])
 
 
     cs2_observations.extend(ak_observations)
-    data = pd.DataFrame(cs2_observations, columns=["Longitude", "Latitude", "Type", "Value", "StdDev", "Month"])
+    data = pd.DataFrame(cs2_observations, columns=["Longitude", "Latitude", "Type", "Value", "StdDev", "Month", "Rho_i"])
 
 
     if render_observations:
@@ -130,7 +190,7 @@ def main(
     
     observations_matrix_subset = data.values
 
-    np.savetxt("observations.txt", observations_matrix_subset, '%5.1f %5.1f %d %5.5f %5.5f %d')
+    np.savetxt("observations.txt", observations_matrix_subset, '%5.1f %5.1f %d %5.5f %5.5f %d %d')
 
     # Add the total number of observations at the top of the file
     with open('observations.txt', 'r') as original: data = original.read()
@@ -155,9 +215,10 @@ def main(
                 "-P", "priors/prior_ice.txt", 
                 "-M", "priors/positionprior_snow.txt", 
                 "-M", "priors/positionprior_ice.txt",
-                "-H", "priors/hierarchical_snow.txt", 
-                "-H", "priors/hierarchical_ice.txt", 
-                "-C", str(initial_cells),
+                #"-H", "priors/hierarchical_snow.txt", 
+                #"-H", "priors/hierarchical_ice.txt", 
+                "-C", str(initial_cells), "-C", str(initial_cells),
+                "-T", str(4000),
                 "-x", str(minlon), "-X", str(maxlon),
                 "-y", str(minlat), "-Y", str(maxlat),
                 "-A", str(parametrization), "-A", str(parametrization),
@@ -207,136 +268,32 @@ def main(
                 #"-V", str(file_ice + "_image"),
                 "-I", str(1)])            
        
-    '''
-    Step 4: Produce and save plots
-    '''
-    # snow_mat = np.loadtxt(file_snow)
-    # ice_mat = np.loadtxt(file_ice)
+## Parameters for the inversion
+date     = ["20210408", "20210409", "20210410", "20210411", "20210412", "20210413", "20210414", "20210415", "20210416", "20210417", "20210418", "20210419",
+            "20210420", "20210421", "20210422"]
+#date = ["20190415"]
+month    = 6 #Number of month since October (ex : 3 for January)
+window = 30
+fb_path1 = "/home/erb/masterproject/MSCI_project/snow_ice/carmen/non_interpolated_data/25km_ease_binned/AK_LARM/dailyFB_25km_2020-2021_season.pkl"
+fb_path2 = "/home/erb/masterproject/MSCI_project/snow_ice/carmen/non_interpolated_data/25km_ease_binned/CS2_LARM/dailyFB_25km_2020-2021_season.pkl"
+verbose=False
+minlat = -5000000.0/1000000
+maxlat = 5000000.0/1000000
+minlon = -5000000.0/1000000
+maxlon = 5000000.0/1000000
+parametrization = 1
+initial_cells = 1500
+iterations_number = 1500000
+verbosity = 5000
+independent_chains = 4
+temperature_levels = 1
+maximum_temperature = 2.0
+iterations_between_tempering_attempts = 10
+skipping = 500000
+thinning = 5
+render_observations=False
 
-    # snow_mat = mask_observations(cs2_mean, snow_mat)
-    # ice_mat = mask_observations(cs2_mean, ice_mat)
-
-
-    # lon = np.linspace(minlon, maxlon, 160)
-    # lat = np.linspace(minlat, maxlat, 160)
-    # lon_g, lat_g = np.meshgrid(lon, lat)
-
-    # lon_g = np.load("will_lons.npy")[:-1, :-1]
-    # lat_g = np.load("will_lats.npy")[:-1, :-1]
-
-    # extent = [minlon, maxlon, minlat, maxlat]
-
-
-    # if render_matrix:
-    #     fig, ax = plt.subplots(1, 2, figsize=(15, 12))
-        
-    #     img = ax[0].imshow(snow_mat, cmap='seismic', aspect='auto', extent=extent, interpolation='None')
-    #     ax[0].set_title('Snow thickness')
-    #     plt.colorbar(img, ax=ax[0])
-
-    #     img = ax[1].imshow(ice_mat, cmap='seismic', aspect='auto', extent=extent, interpolation='None')
-    #     ax[1].set_title('Ice thickness')
-    #     plt.colorbar(img, ax=ax[1])
-
-    #     plt.show()
-
-
-    # if render_map:
-    #     fig = plt.figure(figsize=(16, 20))
-    #     ax = fig.add_subplot(221)
-
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=snow_mat, cmap="seismic")
-    #     plt.colorbar(label=r'Snow Thickness (m)')
-
-    #     ax = fig.add_subplot(222)
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=ice_mat, cmap="seismic")
-    #     plt.colorbar(label=r'Ice Thickness (m)')
-
-
-    #     plt.show()
-
-    # # snow_std = np.loadtxt(file_snow + "_stddev")
-    # # ice_std = np.loadtxt(file_ice + "_stddev")
-
-    # # snow_std = mask_observations(cs2_mean, snow_std)
-    # # ice_std = mask_observations(cs2_mean, ice_std)
-
-
-    # if render_stddev:
-    #     fig = plt.figure(figsize=(16, 20))
-    #     ax = fig.add_subplot(221)
-
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=snow_std, cmap="seismic")
-    #     plt.colorbar(label=r'Snow Thickness Std (m)')
-
-    #     ax = fig.add_subplot(222)
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=ice_std, cmap="seismic")
-    #     plt.colorbar(label=r'Ice Thickness Std (m)')
-
-
-    #     plt.show()
-
-
-    # '''
-    # snow_hist = np.loadtxt(file_snow + "_histogram")
-    # ice_hist = np.loadtxt(file_ice + "_histogram")
-
-    # snow_hist = mask_observations(cs2_mean, snow_hist)
-    # ice_hist = mask_observations(cs2_mean, ice_hist)
-
-    # if render_histogram:
-    #     fig = plt.figure(figsize=(16, 20))
-    #     ax = fig.add_subplot(221)
-
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=snow_hist, cmap="seismic")
-    #     plt.colorbar(label=r'Snow Thickness (m)')
-
-    #     ax = fig.add_subplot(222)
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=ice_hist, cmap="seismic")
-    #     plt.colorbar(label=r'Ice Thickness (m)')
-
-
-    #     plt.show()
-    # '''
-
-    # # snow_median = np.loadtxt(file_snow + "_median")
-    # # ice_median = np.loadtxt(file_ice + "_median")
-
-    # # snow_median = mask_observations(cs2_mean, snow_median)
-    # # ice_median = mask_observations(cs2_mean, ice_median)
-
-
-    # if render_median:
-    #     fig = plt.figure(figsize=(16, 20))
-    #     ax = fig.add_subplot(221)
-
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=snow_median, cmap="seismic")
-    #     plt.colorbar(label=r'Snow Thickness Median (m)')
-
-    #     ax = fig.add_subplot(222)
-    #     m = Basemap(projection='lcc', resolution=None, lat_0=-90, lon_0=0, lat_1=89.9, lon_1=180, width=1E7, height=0.5E7)
-    #     draw_map(m)
-    #     m.scatter(lon_g, lat_g, latlon=True, alpha=1, s=0.5, c=ice_median, cmap="seismic")
-    #     plt.colorbar(label=r'Ice Thickness Median (m)')
-
-
-    #     plt.show()
-
-    # return snow_mat, ice_mat
 
 if __name__ == "__main__":
-    main()
+    main(month, fb_path1, fb_path2, verbose, date, window, minlat, maxlat, minlon, maxlon, parametrization, initial_cells, iterations_number, verbosity, 
+         independent_chains, temperature_levels, maximum_temperature, iterations_between_tempering_attempts, skipping, thinning, render_observations)
